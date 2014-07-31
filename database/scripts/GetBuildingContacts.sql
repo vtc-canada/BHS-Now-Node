dc1360ea6798f4a43db9373ba17acd7c1f545083
wiechars <rwiecha@isystemsnow.com>
2014-07-31 16:56:59 -0400

    Updated sproc after changing the mapping table schema

--- database/scripts/GetBuildingContacts.sql
+++ database/scripts/GetBuildingContacts.sql
@@ -4,30 +4,27 @@ DROP PROCEDURE if EXISTS `GetBuildingContacts` ;
 CREATE PROCEDURE `GetBuildingContacts`(IN buildingID int)
 
 SELECT
-	owner_company.name as 'owner_company_name'
-	,owner_contact.name as 'owner_name'	
-	,owner_phone.phone_number as 'owner_phone'
-	,owner_contact.email as 'owner_email'
+	cur_company.name as 'cur_company_name'
+	,ref_contact_type.type as 'contact_type'
+	,cur_contacts.name as 'contact_name'	
+	,GROUP_CONCAT(DISTINCT cur_phone_numbers.phone_number) as 'phone_number' 
+	,cur_contacts.email 
 	,cur_address.street_number_begin
 	,cur_address.street_number_end
 	,cur_address.street_name
 	,cur_address.postal_code
 	,cur_address.city
 	,cur_address.province
-	,seller_company.name as 'seller_company_name'
-	,seller_contact.name as 'seller_name'	
-	,seller_phone.phone_number as 'seller_phone'
-	,seller_contact.email as 'seller_email'
-
 FROM 
 	cur_buildings
 	INNER JOIN cur_address ON (cur_address.id = cur_buildings.cur_address_id)
 	INNER JOIN cur_owner_seller_property_mapping AS mapping ON (mapping.property_address_id = cur_address.id)
-	LEFT JOIN cur_contacts AS owner_contact ON (owner_contact.id = mapping.owner_contact_id)
-	LEFT JOIN cur_company AS owner_company ON (owner_company.id = mapping.owner_company_id)	
-	LEFT JOIN cur_phone_numbers AS owner_phone ON (owner_phone.contact_ID = owner_contact.id)
-	LEFT JOIN cur_contacts AS seller_contact ON (seller_contact.id = mapping.seller_contact_id)
-	LEFT JOIN cur_company AS seller_company ON (seller_company.id = mapping.seller_company_id)	
-	LEFT JOIN cur_phone_numbers AS seller_phone ON (seller_phone.contact_ID = seller_contact.id)
+	LEFT JOIN cur_contacts  ON (cur_contacts.id = mapping.contact_id)
+	LEFT JOIN cur_company AS cur_company ON (cur_company.id = mapping.company_id)	
+	LEFT JOIN cur_phone_numbers  ON (cur_phone_numbers.contact_ID = cur_contacts.id)
+	INNER JOIN ref_contact_type ON (ref_contact_type.id = mapping.contact_type_id)
 WHERE 
 	cur_buildings.id = buildingID
+GROUP BY	
+	cur_contacts.id;
+	
