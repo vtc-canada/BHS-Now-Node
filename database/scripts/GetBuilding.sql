60e670e8c90fcb50cb413c72196d4b9b4fb60c8e
wiechars <rwiecha@isystemsnow.com>
2014-07-31 16:59:40 -0400

    Updated sproc based on new schema

--- database/scripts/GetBuilding.sql
+++ database/scripts/GetBuilding.sql
@@ -3,8 +3,10 @@ DROP PROCEDURE if EXISTS `GetBuilding` ;
 
 CREATE PROCEDURE `GetBuilding`(IN buildingID int)
 
-SELECT 
-	cur_address.street_number_begin
+SELECT
+	cur_buildings.id as 'building_id'
+	,cur_address.id as 'address_id'
+	,cur_address.street_number_begin
 	,cur_address.street_number_end
 	,cur_address.street_name
 	,cur_address.postal_code
@@ -15,27 +17,33 @@ SELECT
 	,cur_buildings.prev_property_mgmt_company
 	,ref_heat_system_type.type as 'heat_system'
 	,cur_buildings.heat_system_age
-	,ref_window_type.type as 'windows'
-	,cur_buildings.windows_age
-	,ref_elevator_type.type as 'elevators'
-	,cur_buildings.elevators_age
-	,ref_boiler_type.type as 'boilers'
-	,cur_buildings.boiler_age
+	,cur_buildings.windows_installed_year
+	,cur_buildings.elevator_installed_year
+	,cur_buildings.boiler_installed_year
 	,cur_buildings.cable_internet_provider
-	,cur_buildings.unit_quantity
+	,cur_buildings.bachelor_price
+	,cur_buildings.bachelor_units
+	,cur_buildings.bedroom1_price
+	,cur_buildings.bedroom1_units
+	,cur_buildings.bedroom2_price
+	,cur_buildings.bedroom2_price
+	,cur_buildings.bedroom3_price
+	,cur_buildings.bedroom3_units
 	,cur_buildings.assessed_value
-	,cur_buildings.unit_pricing
+	,cur_buildings.building_income
+	,cur_buildings.price_per_unit
 	,cur_buildings.last_sale_price
 	,cur_buildings.sale_date
+	,cur_buildings.images
+	,cur_buildings.has_elevator
+	,cur_buildings.last_elevator_upgrade_year
+	,cur_buildings.last_boiler_upgrade_year
 	
 FROM
 	cur_buildings
 	INNER JOIN cur_address ON (cur_address.id = cur_buildings.cur_address_id)
 	LEFT JOIN ref_building_type ON (cur_buildings.ref_building_type_id = ref_building_type.id)
 	LEFT JOIN ref_heat_system_type ON (ref_heat_system_type.id = cur_buildings.heat_system_type_id)
-	LEFT JOIN ref_window_type ON (ref_window_type.id = cur_buildings.window_type_id)
-	LEFT JOIN ref_elevator_type ON (ref_elevator_type.id = cur_buildings.elevator_type_id)
-	LEFT JOIN ref_boiler_type ON (ref_boiler_type.id = cur_buildings.boiler_type_id)
 
 WHERE
 	cur_buildings.id = buildingID;
