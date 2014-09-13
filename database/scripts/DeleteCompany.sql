75437b860ea901dca4c54e9072ca5ff139dde88c
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2014-09-13 15:44:26 -0400

    Delete contacts/companies renaming. bug fix for bad address mapping

--- database/scripts/DeleteCompany.sql
+++ database/scripts/DeleteCompany.sql
@@ -1,11 +1,12 @@
-USE cred;
+
 DROP PROCEDURE if EXISTS `DeleteCompany` ;
 
 DELIMITER $$
 CREATE PROCEDURE `DeleteCompany`(IN companyID INT)
 BEGIN
 		UPDATE cur_company
-			SET is_deleted = 1
+			SET is_deleted = 1,
+			name = CONCAT(CONCAT(name, '_DELETED_'),NOW())
 		WHERE id = companyID;
 END$$
 DELIMITER ;
--- database/scripts/DeleteContact.sql
+++ database/scripts/DeleteContact.sql
@@ -1,11 +1,12 @@
-USE cred;
+
 DROP PROCEDURE if EXISTS `DeleteContact` ;
 
 DELIMITER $$
 CREATE PROCEDURE `DeleteContact`(IN contactID INT)
 BEGIN
 		UPDATE cur_contacts
-			SET is_deleted = 1
+			SET is_deleted = 1,
+			name = CONCAT(CONCAT(name, '_DELETED_'),NOW())
 		WHERE id = contactID;
 END$$
 DELIMITER ;
