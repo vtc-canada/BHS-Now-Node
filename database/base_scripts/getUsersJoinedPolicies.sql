e38c8d3dd770bf88f690b41fd045fb6345354d79
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2014-09-13 15:05:37 -0400

    unitsize autonumeric issue

--- database/base_scripts/getUsersJoinedPolicies.sql
+++ database/base_scripts/getUsersJoinedPolicies.sql
@@ -5,8 +5,8 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `getUsersJoinedPolicies`()
 BEGIN
 	SELECT users.id
 		,users.username
-		,MAX(CASE securityGroups.name  WHEN 'Administrator' THEN 1 ELSE 0 END) as 'Administrator'
-		,MAX(CASE securityGroups.name  WHEN 'Anonymous' THEN 1 ELSE 0 END) as 'Anonymous'
+		,MAX(CASE securitygroups.name  WHEN 'Administrator' THEN 1 ELSE 0 END) as 'Administrator'
+		,MAX(CASE securitygroups.name  WHEN 'Anonymous' THEN 1 ELSE 0 END) as 'Anonymous'
 	FROM users
 		INNER JOIN userssecuritypolicies ON (userssecuritypolicies.userId = users.id)
 		INNER JOIN securitygroups ON (securitygroups.id = userssecuritypolicies.securityGroupId);
