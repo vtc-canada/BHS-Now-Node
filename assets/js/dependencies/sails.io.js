680b9b10b91a21f18538e1359f32f73cec2eb94a
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2014-10-03 13:37:33 -0400

    Getting layout and translations, configs set up

--- assets/js/dependencies/sails.io.js
+++ assets/js/dependencies/sails.io.js
@@ -725,7 +725,7 @@ var io="undefined"==typeof module?{}:module.exports;(function(){(function(a,b){v
             '`io.socket` connected successfully.' + '\n' +
             // 'e.g. to send a GET request to Sails via WebSockets, run:'+ '\n' +
             // '`io.socket.get("/foo", function serverRespondedWith (body, jwr) { console.log(body); })`'+ '\n' +
-            ' (for help, see: http://sailsjs.org/#!documentation/reference/BrowserSDK/BrowserSDK.html)'
+            ''//' (for help, see: http://sailsjs.org/#!documentation/reference/BrowserSDK/BrowserSDK.html)'
           );
           // consolog('(this app is running in development mode - log messages will be displayed)');
 
