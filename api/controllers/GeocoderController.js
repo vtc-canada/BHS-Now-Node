98b5f5680a4125f8594581d4759eacb64e51e032
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2014-10-24 16:16:55 -0400

    Added Code to maporder addresses, so they can be sorted such that they show up more data-representitively, as in, a marker will always show up near an area if there's more zoomed in.

--- api/controllers/GeocoderController.js
+++ api/controllers/GeocoderController.js
@@ -24,22 +24,120 @@ module.exports = {
      * GeocoderController)
      */
     _config : {},
-    updatecaprate:function(req,res){
-	sails.controllers.database.credQuery('SELECT id, building_income, last_sale_price FROM cur_buildings',function(err,buildings){
-	   if(err)
-	       return res.json({error:'error selecting'+err});
-	   
-	   for(var i=0;i<buildings.length;i++){
-	       if(buildings[i].building_income!=null){
-		   console.log(buildings[i].building_income);
-	       }
-	       console.log(buildings[i].id);
-	   }
+    nullorder:function(req,res){
+	sails.controllers.database.credQuery('UPDATE cur_buildings set order NULL', function(err, resposne) {
+	    if (err)
+		    return res.json({
+			    error : 'error blanking' + err
+		    });
+		
 	});
     },
-    clearzeros : function(req,res){
+    updatemaporder : function(req, res) {
+	var closest = parseInt(req.param('id'));
+	var ordered = null;
+	var coords = null;
+	var countupdated = 0;
+	var orderindex = 0;
+	var geolib = require('geolib');
+	    
+	sails.controllers.database.credQuery('SELECT cur_address.id, latitude, longitude, maporder FROM cur_buildings INNER JOIN cur_address ON cur_address.id = cur_buildings.cur_address_id WHERE latitude IS NOT NULL AND maporder IS NOT NULL', function(err, paramOrdered) {
+		if (err)
+		    return res.json({
+			error : 'error getting coords' + err
+		    });
+		ordered = paramOrdered;
+		for(var i=0;i<ordered.length;i++){
+		    if(ordered[i].maporder>orderindex){
+			orderindex = ordered[i].maporder;
+		    }
+		}
+		sails.controllers.database.credQuery('SELECT cur_address.id, latitude, longitude, maporder FROM cur_buildings INNER JOIN cur_address ON cur_address.id = cur_buildings.cur_address_id WHERE latitude IS NOT NULL AND maporder IS NULL', function(err, paramCoords) {
+		    if (err)
+			return res.json({
+			    error : 'error getting coords' + err
+			});
+		    coords = paramCoords;
+		    
+		    function loopCoords(i){
+			var clearopen = true;
+			for(var j=0;j<ordered.length;j++){
+			    
+			    var distance = geolib.getDistance(
+				    {latitude: ordered[j].latitude, longitude: ordered[j].longitude}, 
+				    {latitude: coords[i].latitude, longitude: coords[i].longitude}
+				);
+			    if(distance<closest){
+				clearopen = false;
+				break;
+			    }
+			}
+			if(clearopen){
+			    orderindex++;
+			    ordered.push({id:coords[i].id,latitude:coords[i].latitude,longitude:coords[i].longitude,order:orderindex});
+			    updateOrder(coords[i].id,orderindex,function(err){
+				if(err){
+				    return res.json('err'+err);
+				}
+				i++;
+				if(i<coords.length){
+				    loopCoords(i);
+				}else{
+				    console.log('done '+countupdated);
+				    return res.json('done '+countupdated);
+				}
+			    });
+			}else{
+			    i++;
+				if(i<coords.length){
+				    loopCoords(i);
+				}else{
+				    console.log('done '+countupdated);
+				    return res.json('done '+countupdated);
+				}
+			}
+		    }
+		    if(coords.length>0){
+			loopCoords(0);
+		    }else{
+			console.log('no coords');
+			    return res.json('done '+countupdated);
+		    }
+		});
+
+
+	});
+	
+	function updateOrder(id,order,cb){
+	    sails.controllers.database.credQuery('UPDATE cur_address SET maporder = '+order+' WHERE id = '+id,function(err,response){
+		if(err){
+		    console.log('err'+err);
+		    cb(err);
+		}
+		countupdated++;
+		cb(null);
+	    })
+	}
+
+    },
+    updatecaprate : function(req, res) {
+	sails.controllers.database.credQuery('SELECT id, building_income, last_sale_price FROM cur_buildings', function(err, buildings) {
+	    if (err)
+		return res.json({
+		    error : 'error selecting' + err
+		});
+
+	    for (var i = 0; i < buildings.length; i++) {
+		if (buildings[i].building_income != null) {
+		    console.log(buildings[i].building_income);
+		}
+		console.log(buildings[i].id);
+	    }
+	});
+    },
+    clearzeros : function(req, res) {
 	sails.controllers.database.credQuery('UPDATE cur_address SET latitude = NULL, longitude = NULL WHERE latitude = 0', function(err, results) {
-	    if(err){
+	    if (err) {
 		res.json(err.toString());
 	    }
 	    res.json(results);
@@ -48,7 +146,7 @@ module.exports = {
     batch : function(req, res) {
 	// sails
 	var count = 50;
-	if(typeof(req.params.id)!='undefined'){
+	if (typeof (req.params.id) != 'undefined') {
 	    count = parseInt(req.params.id);
 	}
 	var geocoderProvider = 'google';
@@ -67,63 +165,73 @@ module.exports = {
 	// console.log(res);
 	// });
 
-	sails.controllers.database.credQuery('SELECT * FROM cur_address WHERE street_name IS NOT NULL AND  postal_code IS NOT NULL AND latitude IS NULL ORDER BY id', function(err, results) {
-	    if (err)
-		return res.json({
-		    error : 'Database Error:' + err
-		});
-	    function doLoop(i) {
-		var address = results[i];
-		if(typeof(address)=='undefined'){
-		    return res.json({success:'Ran out of null addresses on index:'+i});
-		}
-		var street_number_end = (address.street_number_end == null || address.street_number_end == 0) ? '' : ' ' + address.street_number_end;
-		var addressSearch = address.street_number_begin + " " + street_number_end + " " + address.street_name + ', ' + address.city + ', '
-			+ address.province + ', Canada, ' + address.postal_code;
-
-		geocoder.geocode(addressSearch, function(err, resgeo) {
-		    if(err){
-
-			if(err.toString()=='Error: Status is ZERO_RESULTS.'){
-			    console.log('zero results');
-			}else{
-			    return res.json({error:'error geocoding'+err});
+	sails.controllers.database.credQuery(
+		'SELECT * FROM cur_address WHERE street_name IS NOT NULL AND  postal_code IS NOT NULL AND latitude IS NULL ORDER BY id',
+		function(err, results) {
+		    if (err)
+			return res.json({
+			    error : 'Database Error:' + err
+			});
+		    function doLoop(i) {
+			var address = results[i];
+			if (typeof (address) == 'undefined') {
+			    return res.json({
+				success : 'Ran out of null addresses on index:' + i
+			    });
 			}
-		    }
-		    console.log(resgeo);
-		    if (typeof (resgeo) != 'undefined' && resgeo.length > 0) {
-			sails.controllers.database.credQuery('UPDATE cur_address SET latitude = ' + resgeo[0].latitude + ', longitude = ' + resgeo[0].longitude
-				+ ' WHERE id = ' + results[i].id, function(err, add) {
-			    //console.log(add);
+			var street_number_end = (address.street_number_end == null || address.street_number_end == 0) ? '' : ' ' + address.street_number_end;
+			var addressSearch = address.street_number_begin + " " + street_number_end + " " + address.street_name + ', ' + address.city + ', '
+				+ address.province + ', Canada, ' + address.postal_code;
 
-				i++;
-				if(i>count){
-				    return res.json({success:'success'});
-				}
-				setTimeout(function(){
-					doLoop(i);
-				},300);
-			});
-		    }else{
-			sails.controllers.database.credQuery('UPDATE cur_address SET latitude = ' + 0 + ', longitude = ' + 0
-				+ ' WHERE id = ' + results[i].id, function(err, add) {
-			    //console.log(add);
+			geocoder.geocode(addressSearch, function(err, resgeo) {
+			    if (err) {
 
-				i++;
-				if(i>count){
-				    return res.json({success:'success'});
+				if (err.toString() == 'Error: Status is ZERO_RESULTS.') {
+				    console.log('zero results');
+				} else {
+				    return res.json({
+					error : 'error geocoding' + err
+				    });
 				}
-				setTimeout(function(){
+			    }
+			    console.log(resgeo);
+			    if (typeof (resgeo) != 'undefined' && resgeo.length > 0) {
+				sails.controllers.database.credQuery('UPDATE cur_address SET latitude = ' + resgeo[0].latitude + ', longitude = '
+					+ resgeo[0].longitude + ' WHERE id = ' + results[i].id, function(err, add) {
+				    // console.log(add);
+
+				    i++;
+				    if (i > count) {
+					return res.json({
+					    success : 'success'
+					});
+				    }
+				    setTimeout(function() {
 					doLoop(i);
-				},300);
+				    }, 300);
+				});
+			    } else {
+				sails.controllers.database.credQuery('UPDATE cur_address SET latitude = ' + 0 + ', longitude = ' + 0 + ' WHERE id = '
+					+ results[i].id, function(err, add) {
+				    // console.log(add);
+
+				    i++;
+				    if (i > count) {
+					return res.json({
+					    success : 'success'
+					});
+				    }
+				    setTimeout(function() {
+					doLoop(i);
+				    }, 300);
+				});
+			    }
 			});
+
 		    }
+		    doLoop(0);
+
 		});
-
-	    }
-	    doLoop(0);
-
-	});
 
 	/*
 	 * var address = { street_number_begin:25,street_number_end :1406,
@@ -141,7 +249,7 @@ module.exports = {
     batchmore : function(req, res) {
 	// sails
 	var count = 50;
-	if(typeof(req.params.id)!='undefined'){
+	if (typeof (req.params.id) != 'undefined') {
 	    count = parseInt(req.params.id);
 	}
 	var geocoderProvider = 'google';
@@ -160,62 +268,71 @@ module.exports = {
 	// console.log(res);
 	// });
 
-	sails.controllers.database.credQuery('SELECT * FROM cur_address WHERE street_name IS NOT NULL AND city IS NOT NULL AND latitude IS NULL ORDER BY id', function(err, results) {
-	    if (err)
-		return res.json({
-		    error : 'Database Error:' + err
-		});
-	    function doLoop(i) {
-		var address = results[i];
-		if(typeof(address)=='undefined'){
-		    return res.json({success:'Ran out of null addresses on index:'+i});
-		}
-		var street_number_end = (address.street_number_end == null || address.street_number_end == 0) ? '' : ' ' + address.street_number_end;
-		var addressSearch = address.street_number_begin + " " + street_number_end + " " + address.street_name + ', ' + address.city + ', '
-			+ address.province + ', Canada, ' + address.postal_code;
-
-		geocoder.geocode(addressSearch, function(err, resgeo) {
-		    if(err){
-			if(err.toString()=='Error: Status is ZERO_RESULTS.'){
-			    console.log('zero results');
-			}else{
-			    return res.json({error:'error geocoding'+err});
+	sails.controllers.database.credQuery('SELECT * FROM cur_address WHERE street_name IS NOT NULL AND city IS NOT NULL AND latitude IS NULL ORDER BY id',
+		function(err, results) {
+		    if (err)
+			return res.json({
+			    error : 'Database Error:' + err
+			});
+		    function doLoop(i) {
+			var address = results[i];
+			if (typeof (address) == 'undefined') {
+			    return res.json({
+				success : 'Ran out of null addresses on index:' + i
+			    });
 			}
-		    }
-		    console.log(resgeo);
-		    if (typeof (resgeo) != 'undefined' && resgeo.length > 0) {
-			sails.controllers.database.credQuery('UPDATE cur_address SET latitude = ' + resgeo[0].latitude + ', longitude = ' + resgeo[0].longitude
-				+ ' WHERE id = ' + results[i].id, function(err, add) {
-			    //console.log(add);
+			var street_number_end = (address.street_number_end == null || address.street_number_end == 0) ? '' : ' ' + address.street_number_end;
+			var addressSearch = address.street_number_begin + " " + street_number_end + " " + address.street_name + ', ' + address.city + ', '
+				+ address.province + ', Canada, ' + address.postal_code;
 
-				i++;
-				if(i>count){
-				    return res.json({success:'success'});
+			geocoder.geocode(addressSearch, function(err, resgeo) {
+			    if (err) {
+				if (err.toString() == 'Error: Status is ZERO_RESULTS.') {
+				    console.log('zero results');
+				} else {
+				    return res.json({
+					error : 'error geocoding' + err
+				    });
 				}
-				setTimeout(function(){
-					doLoop(i);
-				},300);
-			});
-		    }else{
-			sails.controllers.database.credQuery('UPDATE cur_address SET latitude = ' + 0 + ', longitude = ' + 0
-				+ ' WHERE id = ' + results[i].id, function(err, add) {
-			    //console.log(add);
+			    }
+			    console.log(resgeo);
+			    if (typeof (resgeo) != 'undefined' && resgeo.length > 0) {
+				sails.controllers.database.credQuery('UPDATE cur_address SET latitude = ' + resgeo[0].latitude + ', longitude = '
+					+ resgeo[0].longitude + ' WHERE id = ' + results[i].id, function(err, add) {
+				    // console.log(add);
 
-				i++;
-				if(i>count){
-				    return res.json({success:'success'});
-				}
-				setTimeout(function(){
+				    i++;
+				    if (i > count) {
+					return res.json({
+					    success : 'success'
+					});
+				    }
+				    setTimeout(function() {
 					doLoop(i);
-				},300);
+				    }, 300);
+				});
+			    } else {
+				sails.controllers.database.credQuery('UPDATE cur_address SET latitude = ' + 0 + ', longitude = ' + 0 + ' WHERE id = '
+					+ results[i].id, function(err, add) {
+				    // console.log(add);
+
+				    i++;
+				    if (i > count) {
+					return res.json({
+					    success : 'success'
+					});
+				    }
+				    setTimeout(function() {
+					doLoop(i);
+				    }, 300);
+				});
+			    }
 			});
+
 		    }
+		    doLoop(0);
+
 		});
-
-	    }
-	    doLoop(0);
-
-	});
 
 	/*
 	 * var address = { street_number_begin:25,street_number_end :1406,
@@ -233,7 +350,7 @@ module.exports = {
     batchblockpostal : function(req, res) {
 	// sails
 	var count = 50;
-	if(typeof(req.params.id)!='undefined'){
+	if (typeof (req.params.id) != 'undefined') {
 	    count = parseInt(req.params.id);
 	}
 	var geocoderProvider = 'google';
@@ -252,62 +369,71 @@ module.exports = {
 	// console.log(res);
 	// });
 
-	sails.controllers.database.credQuery('SELECT * FROM cur_address WHERE street_name IS NOT NULL AND city IS NOT NULL AND latitude IS NULL ORDER BY id', function(err, results) {
-	    if (err)
-		return res.json({
-		    error : 'Database Error:' + err
-		});
-	    function doLoop(i) {
-		var address = results[i];
-		if(typeof(address)=='undefined'){
-		    return res.json({success:'Ran out of null addresses on index:'+i});
-		}
-		var street_number_end = (address.street_number_end == null || address.street_number_end == 0) ? '' : ' ' + address.street_number_end;
-		var addressSearch = address.street_number_begin + " " + street_number_end + " " + address.street_name + ', ' + address.city + ', '
-			+ address.province + ', Canada';
-
-		geocoder.geocode(addressSearch, function(err, resgeo) {		    
-		    if(err){
-			if(err.toString()=='Error: Status is ZERO_RESULTS.'){
-			    console.log('zero results');
-			}else{
-			    return res.json({error:'error geocoding'+err});
+	sails.controllers.database.credQuery('SELECT * FROM cur_address WHERE street_name IS NOT NULL AND city IS NOT NULL AND latitude IS NULL ORDER BY id',
+		function(err, results) {
+		    if (err)
+			return res.json({
+			    error : 'Database Error:' + err
+			});
+		    function doLoop(i) {
+			var address = results[i];
+			if (typeof (address) == 'undefined') {
+			    return res.json({
+				success : 'Ran out of null addresses on index:' + i
+			    });
 			}
-		    }
-		    console.log(resgeo);
-		    if (typeof (resgeo) != 'undefined' && resgeo.length > 0) {
-			sails.controllers.database.credQuery('UPDATE cur_address SET latitude = ' + resgeo[0].latitude + ', longitude = ' + resgeo[0].longitude
-				+ ' WHERE id = ' + results[i].id, function(err, add) {
-			    //console.log(add);
+			var street_number_end = (address.street_number_end == null || address.street_number_end == 0) ? '' : ' ' + address.street_number_end;
+			var addressSearch = address.street_number_begin + " " + street_number_end + " " + address.street_name + ', ' + address.city + ', '
+				+ address.province + ', Canada';
 
-				i++;
-				if(i>count){
-				    return res.json({success:'success'});
+			geocoder.geocode(addressSearch, function(err, resgeo) {
+			    if (err) {
+				if (err.toString() == 'Error: Status is ZERO_RESULTS.') {
+				    console.log('zero results');
+				} else {
+				    return res.json({
+					error : 'error geocoding' + err
+				    });
 				}
-				setTimeout(function(){
-					doLoop(i);
-				},300);
-			});
-		    }else{
-			sails.controllers.database.credQuery('UPDATE cur_address SET latitude = ' + 0 + ', longitude = ' + 0
-				+ ' WHERE id = ' + results[i].id, function(err, add) {
-			    //console.log(add);
+			    }
+			    console.log(resgeo);
+			    if (typeof (resgeo) != 'undefined' && resgeo.length > 0) {
+				sails.controllers.database.credQuery('UPDATE cur_address SET latitude = ' + resgeo[0].latitude + ', longitude = '
+					+ resgeo[0].longitude + ' WHERE id = ' + results[i].id, function(err, add) {
+				    // console.log(add);
 
-				i++;
-				if(i>count){
-				    return res.json({success:'success'});
-				}
-				setTimeout(function(){
+				    i++;
+				    if (i > count) {
+					return res.json({
+					    success : 'success'
+					});
+				    }
+				    setTimeout(function() {
 					doLoop(i);
-				},300);
+				    }, 300);
+				});
+			    } else {
+				sails.controllers.database.credQuery('UPDATE cur_address SET latitude = ' + 0 + ', longitude = ' + 0 + ' WHERE id = '
+					+ results[i].id, function(err, add) {
+				    // console.log(add);
+
+				    i++;
+				    if (i > count) {
+					return res.json({
+					    success : 'success'
+					});
+				    }
+				    setTimeout(function() {
+					doLoop(i);
+				    }, 300);
+				});
+			    }
 			});
+
 		    }
+		    doLoop(0);
+
 		});
-
-	    }
-	    doLoop(0);
-
-	});
 
 	/*
 	 * var address = { street_number_begin:25,street_number_end :1406,
