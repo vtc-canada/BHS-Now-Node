c920dbdc27b374989e351245340ab95216812883
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2014-10-27 11:35:08 -0400

    Configuration location changes for locales and reports.

---
+++ config/locale_en.js
@@ -0,0 +1,107 @@
+module.exports.views = {
+    locals : {
+	translate : {
+	    'en' : {
+		//Login Screen
+		'DefaultTitle' : 'BHS Now',
+		'Login Title' : 'Manage your Baggage Handling System',
+		'You are not logged into the system. Please log in.' : 'You are not signed into the system. Your session may have automatically expired. Please sign in.',
+		'This account does not have access privileges' : 'This account does not have access privileges.',
+		'Main Title' : 'Manage your Real Estate.',
+		'Sign in' : 'Sign in',
+		'Change Password' : 'Change Password',
+		'Sign Out' : 'Sign Out',
+		'User' : 'User',
+		'Search' : 'Search',
+		'Messages' : 'Messages',
+		'Password' : 'Password',
+		'Wrong Password' : 'The username or password you have entered is incorrect.',
+		'User not Found' : 'The username or password you have entered is incorrect.',
+		'Username and Password is Required' : 'A username and Password are required.',
+		'Messaging' : 'Messaging',
+		'Search people' : 'Search people',
+		'DB Error' : 'Database Error.',
+		'FormError' : 'Error',
+		'FormSuccess' : 'Success',
+		'Password fields do not match.' : 'Password fields do not match.',
+		'Password must be at least 1 character long.' : 'Password must be at least 1 character long.',
+		'Enter a new password for' : 'Enter a new password for',
+		'Current Password' : 'Current Password',
+		'New Password' : 'New Password',
+		'Confirm Password' : 'Confirm Password',
+		'FormCancel' : 'Cancel',
+		'FormSet' : 'Set',
+		'Current password was incorrect.' : 'Current password was incorrect.',
+		'Password has been successfully changed.' : 'Password has been successfully changed.',
+
+		//Chat boxes
+		'Load Earlier Messages' : 'Load Earlier Messages',
+		'End of messages' : 'End of messages',
+
+		//Messages Screen
+		'Messaging' : 'Messaging',
+		'Users' : 'Users',
+		'Inbox' : 'Inbox',
+		'Message User' : 'Message User',
+
+		//Users Screen My Account Popover
+		'Users Administration' : 'Users Administration',
+		'Users' : 'Users',
+		'Create New User' : 'Create New',
+		'Create User' : 'Create User',
+		'Edit' : 'Edit',
+		'Invalid Username' : 'Invalid Username',
+		'User Saved' : 'User Saved',
+		'User Deleted' : 'User Deleted',
+		'Password Mismatch' : 'New Password and Confirm Password fields did not match.',
+
+		//Security Screen
+		'LevelCreate' : 'Create',
+		'LevelRead' : 'Read',
+		'LevelUpdate' : 'Update',
+		'LevelDelete' : 'Delete',
+		'SecurityResource' : 'Resource',
+		'Create Security Group' : 'Create Security Group',
+		'Invalid Security Group Name' : 'Invalid Security Group Name',
+		'Security Group Saved' : 'Security Group Saved',
+		'Security Group Deleted' : 'Security Group Deleted',
+
+		// Dashboard
+
+		'You do not have sufficient privileges to access' : 'You do not have sufficient privileges to access',
+		'Dashboard' : 'Dashboard',
+		'Date' : 'Date',
+		'Equipment' : 'Equipment',
+		'Device' : 'Device',
+		'Alarm Type' : 'Alarm Type',
+		'System Health' : 'System Health',
+		'System Throughput' : 'System Throughput',
+		'Time In System' : 'Time In System',
+		'BHS Overview' : 'BHS Overview',
+		'Time' : 'Time',
+		'Alarms' : 'Alarms',
+		'Daily Reminders' : 'Daily Reminders',
+		'Predictive Mainenance Tasks' : 'Predictive Mainenance Tasks',
+		'DEVICE' : 'DEVICE',
+		'REASON' : 'REASON',
+		'Uptime' : 'Uptime',
+		'Failsafe Rate' : 'Failsafe Rate',
+		'Tracking Rate' : 'Tracking Rate',
+		'Key Performance Indicators' : 'Key Performance Indicators',
+		'Throughput (bph)' : 'Volume (total bags)',
+		'State' : 'State',
+		'Jam Rate' : 'Jam Rate',
+		'ATR Read Rate' : 'ATR Read Rate',
+
+		'pages' : {
+		    '/' : 'test',
+		    '/dashboard' : 'Dashboard',
+		    '/systemconfig' : 'System Config',
+		    '/admin/users' : 'Users',
+		    '/admin/securitygroups' : 'Security Groups',
+		    '/reports' : 'Reports'
+		}
+	    }
+	}
+    }
+}
