509ad05be0cb5f6fab92a1925e57002042c03188
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2014-10-27 11:35:51 -0400

    Initial Reports Setup Commit

---
+++ api/controllers/ReportsController.js
@@ -0,0 +1,12 @@
+/**
+ * ReportsController
+ *
+ * @description :: Server-side logic for managing reports
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+    index : function(req, res) {
+	res.view();
+    }
+};
