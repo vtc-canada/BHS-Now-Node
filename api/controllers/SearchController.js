86e3f7d8a757d11ece7763e3c765152a49933fa1
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2014-10-27 15:54:19 -0400

    Added node-phantom dependency manually- (as it has custom code changes from the default module)

---
+++ api/controllers/SearchController.js
@@ -0,0 +1,11 @@
+/**
+ * SearchController
+ *
+ * @description :: Server-side logic for managing searches
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
