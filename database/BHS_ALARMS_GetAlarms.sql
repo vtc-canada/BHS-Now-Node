aa2e32281c53ea6ef73154146894578d2d189a03
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2014-11-04 17:06:26 -0500

    Added Color to Alarms on Dashboard. Added Alarms to Overview screen. Created overview screen generic positioning, rotation for straight/turn(90s) for modern and ie8 browsers.  Degraded some borders where possible. Legend to overview screen.

--- database/BHS_ALARMS_GetAlarms.sql
+++ database/BHS_ALARMS_GetAlarms.sql
@@ -1,5 +1,5 @@
 DROP PROCEDURE if EXISTS `BHS_ALARMS_GetAlarms` ;
-
+DELIMITER $$
 CREATE PROCEDURE  `BHS_ALARMS_GetAlarms` () 
 BEGIN
 	SELECT cur_alarms.id
@@ -9,9 +9,14 @@ BEGIN
 		,cfg_dev_id.name AS device
 		,ref_alarms_def.text
 		,ref_alarms_def.severity
+		,ref_main_color_def.color_code as 'background_color'
+		,ref_text_color_def.color_code as 'text_color'
 	FROM cur_alarms 
 	INNER JOIN cfg_tag_id ON cur_alarms.tag_ID = cfg_tag_id.id 
-	INNER JOIN ref_alarms_def ON ref_alarms_def.id = cfg_tag_id.alarm_type 
+	INNER JOIN ref_alarms_def ON ref_alarms_def.id = cfg_tag_id.alarm_type
+	INNER JOIN ref_status_def ON ref_status_def.id = ref_alarms_def.status
+	INNER JOIN ref_color_def AS ref_main_color_def ON ref_color_def.id = ref_status_def.color_ID
+	INNER JOIN ref_color_def AS ref_text_color_def ON ref_color_def.id = ref_status_def.text_color_ID
 	LEFT OUTER JOIN cfg_dev_id ON cfg_dev_id.id = cfg_tag_id.dev_ID 
 	LEFT OUTER JOIN cfg_eqp_id ON cfg_eqp_id.id = cfg_tag_id.eqp_ID;
-END
+END $$
