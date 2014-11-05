202a2d3e21faef3a322ce817a78e31795f8e43da
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2014-11-05 12:45:00 -0500

    Script Modifications

--- database/base_scripts/createResourceSecurityGroupMapping.sql
+++ database/base_scripts/createResourceSecurityGroupMapping.sql
@@ -1,15 +1,12 @@
-USE `now_management_base`;
+
 DROP procedure IF EXISTS `createResourceSecurityGroupMapping`;
 
-DELIMITER $$
-USE `now_management_base`$$
 CREATE PROCEDURE `createResourceSecurityGroupMapping` (IN paramResourceId INT(11),IN paramSecurityGroupId INT(11),IN paramCreate TINYINT(1),IN paramRead TINYINT(1),IN paramUpdate TINYINT(1),IN paramDelete TINYINT(1))
 BEGIN
 	INSERT INTO resourcesecuritygroupmappings 
 (resourceId, securityGroupId, resourcesecuritygroupmappings.create, resourcesecuritygroupmappings.read, resourcesecuritygroupmappings.update, resourcesecuritygroupmappings.delete, createdAt, updatedAt) 
 VALUES 
 (paramResourceId, paramSecurityGroupId, paramCreate, paramRead, paramUpdate, paramDelete, UTC_TIMESTAMP(), UTC_TIMESTAMP());
-END$$
+END
 
-DELIMITER ;
 
