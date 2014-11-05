2c0ea349380a4ef5481fa4babdd3c7b4e7f80f2e
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2014-11-05 15:18:49 -0500

    Fixed Bug with GetAlarms  sproc

--- database/BHS_ALARMS_GetAlarms.sql
+++ database/BHS_ALARMS_GetAlarms.sql
@@ -15,8 +15,8 @@ BEGIN
 	INNER JOIN cfg_tag_id ON cur_alarms.tag_ID = cfg_tag_id.id 
 	INNER JOIN ref_alarms_def ON ref_alarms_def.id = cfg_tag_id.alarm_type
 	INNER JOIN ref_status_def ON ref_status_def.id = ref_alarms_def.status
-	INNER JOIN ref_color_def AS ref_main_color_def ON ref_color_def.id = ref_status_def.color_ID
-	INNER JOIN ref_color_def AS ref_text_color_def ON ref_color_def.id = ref_status_def.text_color_ID
+	INNER JOIN ref_color_def AS ref_main_color_def ON ref_main_color_def.id = ref_status_def.color_ID
+	INNER JOIN ref_color_def AS ref_text_color_def ON ref_text_color_def.id = ref_status_def.text_color_ID
 	LEFT OUTER JOIN cfg_dev_id ON cfg_dev_id.id = cfg_tag_id.dev_ID 
 	LEFT OUTER JOIN cfg_eqp_id ON cfg_eqp_id.id = cfg_tag_id.eqp_ID;
 END $$
