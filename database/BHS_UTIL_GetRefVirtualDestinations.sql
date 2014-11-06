dbe1b907dd65a2142efdb0eae127188527a801db
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2014-11-06 15:21:56 -0500

    Fixed sprocs with 'USE bhs_scada' CP to bhs

--- database/BHS_UTIL_GetRefVirtualDestinations.sql
+++ database/BHS_UTIL_GetRefVirtualDestinations.sql
@@ -1,8 +1,7 @@
-USE `bhs_scada`;
+
 DROP procedure IF EXISTS `BHS_UTIL_GetRefVirtualDestinations`;
 
 DELIMITER $$
-USE `bhs_scada`$$
 CREATE PROCEDURE `BHS_UTIL_GetRefVirtualDestinations` ()
 BEGIN
 	SELECT * FROM ref_virtual_destinations;
