47efca2c73c063d5051b97d12a5fc091b7507ac5
wiechars <rwiecha@isystemsnow.com>
2014-12-11 10:33:07 -0500

    Updated Contact Sprocs

--- database/CreateContact.sql
+++ database/CreateContact.sql
@@ -3,11 +3,30 @@ DROP PROCEDURE if EXISTS `CreateContact` ;
 DELIMITER $$
 CREATE PROCEDURE `CreateContact`(IN firstName VARCHAR(256),IN middleName VARCHAR(256), IN lastName VARCHAR(256),IN email VARCHAR(64), 
 				IN phoneNumber VARCHAR(64),IN dateOfBirth DATE,IN driversLicense VARCHAR(64),IN passport VARCHAR(64),
-				IN nationality VARCHAR(64),OUT id INT)
+				IN nationality VARCHAR(64), IN gender VARCHAR(64),OUT id INT)
 BEGIN
-	INSERT INTO cur_contacts(first_name,middle_name,last_name,email,phone_number,date_of_birth,drivers_license,passport_no,
-			nationality,is_deleted) 
-	VALUES (firstName,middleName,lastName,email,phoneNumber,dateOfBirth,driversLicense,passport,nationality, 0);
+	INSERT INTO cur_contacts(first_name
+		,middle_name
+		,last_name
+		,email
+		,phone_number
+		,date_of_birth
+		,drivers_license
+		,passport_no
+		,nationality
+		,gender
+		,is_deleted) 
+	VALUES (firstName
+		,middleName
+		,lastName
+		,email
+		,phoneNumber
+		,dateOfBirth
+		,driversLicense
+		,passport
+		,nationality
+		,gender
+		,0);
 	SET id = LAST_INSERT_ID();
 END$$
 DELIMITER ;
--- database/UpdateContact.sql
+++ database/UpdateContact.sql
@@ -1,13 +1,21 @@
 DROP PROCEDURE if EXISTS `UpdateContact` ;
 
 DELIMITER $$
-CREATE PROCEDURE `UpdateContact`(IN contactID INT, IN firstName VARCHAR(256), IN lastName VARCHAR(256), IN email VARCHAR(64), IN phoneNumber VARCHAR(64))
+CREATE PROCEDURE `UpdateContact`(IN contactID INT, IN firstName VARCHAR(256), IN middleName VARCHAR(256),
+			IN lastName VARCHAR(256), IN email VARCHAR(64), IN phoneNumber VARCHAR(64),
+			IN dateOfBirth DATE,IN driversLicense VARCHAR(64),IN passport VARCHAR(64),IN nationality VARCHAR(64), IN gender VARCHAR(64))
 BEGIN
 	UPDATE cur_contacts
 		SET first_name = firstName
+		,middle_name = middleName
 		,last_name = lastName
 		,email = email
 		,phone_number = phoneNumber
+		,date_of_birth = dateOfBirth
+		,drivers_license = driversLicense
+		,passport_no = passport
+		,nationality = nationality
+		,gender = gender
 	WHERE
 		id = contactID;
 END$$
