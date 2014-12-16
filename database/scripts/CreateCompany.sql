1d3ee9d58756cf1561c09671811daecdf6a3369a
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2014-12-16 16:27:41 -0500

    ECP-003  Sprocs
    Added phone_number field.

--- database/scripts/CreateCompany.sql
+++ database/scripts/CreateCompany.sql
@@ -2,9 +2,9 @@ USE cred;
 DROP PROCEDURE if EXISTS `CreateCompany` ;
 
 DELIMITER $$
-CREATE PROCEDURE `CreateCompany`(IN companyName VARCHAR(128), OUT id INT)
+CREATE PROCEDURE `CreateCompany`(IN companyName VARCHAR(128), IN paramPhone_number VARCHAR(512), OUT id INT)
 BEGIN
-	INSERT INTO cur_company(name) VALUES (companyName);
+	INSERT INTO cur_company(name,phone_number) VALUES (companyName,paramPhone_number);
 	SET id = LAST_INSERT_ID();
 END$$
 DELIMITER ;
