54dd74bdd826021d5fe3a8a2781616d62c6d6d0f
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2014-12-16 16:19:48 -0500

    ECP-003   Sproc
    Added phone_number.

--- database/scripts/UpdateCompany.sql
+++ database/scripts/UpdateCompany.sql
@@ -2,11 +2,12 @@ USE cred;
 DROP PROCEDURE if EXISTS `UpdateCompany` ;
 
 DELIMITER $$
-CREATE PROCEDURE `UpdateCompany`(IN companyID INT, IN companyName VARCHAR(128))
+CREATE PROCEDURE `UpdateCompany`(IN companyID INT, IN companyName VARCHAR(128), IN paramPhone_number VARCHAR(512))
 BEGIN
 	UPDATE cur_company
 		SET
-			name = companyName
+			name = companyName,
+			phone_number = paramPhone_number
 	WHERE cur_company.id = companyID;
 END$$
 DELIMITER ;
