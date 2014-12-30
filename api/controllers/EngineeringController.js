71a26eb8e201e398e6cc5145e3b9d558db8224e9
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2014-12-30 13:06:15 -0500

    Fixed bad link on Register Reset.

--- api/controllers/EngineeringController.js
+++ api/controllers/EngineeringController.js
@@ -81,7 +81,7 @@ module.exports = {
     resetregisters : function(req, res) {
 
 	var net = require('net');
-	var connection = net.connect(sails.config.ECMConn.Port, sails.config.ECMConn.Host);// , function() {
+	var connection = net.connect(sails.config.connections.ECMConn.Port, sails.config.connections.ECMConn.Host);// , function() {
 	connection.on('error', function(err) {
 	    console.log("ECM Client Connection failed." + err.toString());
 	});
