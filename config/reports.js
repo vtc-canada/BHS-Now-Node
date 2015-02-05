cbc6a4f12a0ab6a51e86c541f91d07f2434dc11c
wiechars <rwiecha@isystemsnow.com>
2015-02-05 11:05:17 -0500

    Footers need to be updated to accurately describe what the data in the report is showing. Conflicts: 	api/controllers/ReportsController.js

--- config/reports.js
+++ config/reports.js
@@ -9,6 +9,24 @@ module.exports.views = {
 			es : 'Informe del historial de alarmas'
 		    }
 		},
+		footnote1 : {
+		    locale_label : {
+		    	en : 'Alarm History Report',
+				es : 'Informe del historial de alarmas'
+			    }
+			},
+		footnote2 : {
+		    locale_label : {
+				en : 'Summary of device faults and alarms and their duration for a given time range.',
+				es : ''
+			    }
+			},
+		footnote3 : {
+		    locale_label : {
+				en : '',
+				es : ''
+			    }
+			},
 		title:{
 		    logo:'iSystemsNow-Logo-RGB-Black.png'
 		},
@@ -67,6 +85,24 @@ module.exports.views = {
 			es : 'Informe resumido Equipo'
 		    }
 		},
+		footnote1 : {
+		    locale_label : {
+				en : 'Equipment Summary Report',
+				es : 'Informe resumido Equipo'
+			    }
+			},
+		footnote2 : {
+		    locale_label : {
+				en : 'A summary of equipment statistics for the given time range.',
+				es : ''
+			    }
+			},
+		footnote3 : {
+		    locale_label : {
+				en : '',
+				es : ''
+			    }
+			},
 		title:{
 		    logo:'iSystemsNow-Logo-RGB-Black.png'
 		},
@@ -152,6 +188,24 @@ module.exports.views = {
 			es : 'Informe resumido Equipo'
 		    }
 		},
+		footnote1 : {
+		    locale_label : {
+				en : 'Equipment Interval Report',
+				es : 'Informe resumido Equipo'
+			    }
+			},
+		footnote2 : {
+		    locale_label : {
+				en : 'A summary of cumulative equipment statistics grouped by a time interval and bounded by a time range',
+				es : ''
+			    }
+			},
+		footnote3 : {
+		    locale_label : {
+				en : '',
+				es : ''
+			    }
+			},
 		title:{
 		    logo:'iSystemsNow-Logo-RGB-Black.png'
 		},
@@ -246,6 +300,24 @@ module.exports.views = {
 			es : 'Throughput Report'
 		    }
 		},
+		footnote1 : {
+		    locale_label : {
+				en : 'Throughput Report',
+				es : 'Throughput Report'
+			    }
+			},
+		footnote2 : {
+		    locale_label : {
+				en : ' Input = Bags seen at OB1-03, Output = Bags seen at OB1-07',
+				es : ''
+			    }
+			},
+		footnote3 : {
+		    locale_label : {
+				en : 'Statistics are tracked in the PLC by tags S_OUTPUT_TP_CNT (Output) and S_INPUT_TP_CNT (Input)',
+				es : ''
+			    }
+			},
 		title:{
 		    logo:'iSystemsNow-Logo-RGB-Black.png'
 		},
@@ -286,6 +358,24 @@ module.exports.views = {
 			es : 'Executive Summary Report'
 		    }
 		},
+		footnote1 : {
+		    locale_label : {
+				en : 'Executive Summary Report',
+				es : 'Executive Summary Report'
+			    }
+			},
+		footnote2 : {
+		    locale_label : {
+				en : 'Summary of all statistics for the given day.',
+				es : ''
+			    }
+			},
+		footnote3 : {
+		    locale_label : {
+				en : 'System Availability = (Time Elapsed since Midnight - Cummulative Fault Time) / Time Elapsed since midnight)',
+				es : ''
+			    }
+		},
 		title:{
 		    logo:'iSystemsNow-Logo-RGB-Black.png'
 		},
@@ -316,6 +406,24 @@ module.exports.views = {
 			en : 'Bag Search',
 			es : 'Informe del historial de Bags'
 		    }
+		},
+		footnote1 : {
+		    locale_label : {
+				en : 'Bag Search',
+				es : 'Informe del historial de Bags'
+			    }
+			},
+		footnote2 : {
+		    locale_label : {
+				en : '',
+				es : ''
+			    }
+			},
+		footnote3 : {
+		    locale_label : {
+				en : '',
+				es : ''
+			    }
 		},
 		title:{
 		    logo:'iSystemsNow-Logo-RGB-Black.png'
