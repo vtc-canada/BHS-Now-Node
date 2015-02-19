a5519227a3c7a32d82d1dc103b0d6b4c4079b8a2
wiechars <rwiecha@isystemsnow.com>
2015-02-19 12:59:50 -0500

    Removing queries from project that are not needed.

--- database/BHS_FLIGHTS_DeleteCurFlightSortPlanByFlightAndDate.sql
+++
@@ -1,8 +0,0 @@
-DROP PROCEDURE if EXISTS `BHS_FLIGHTS_DeleteCurFlightSortPlanByFlightAndDate` ;
-
-CREATE PROCEDURE `BHS_FLIGHTS_DeleteCurFlightSortPlanByFlightAndDate`(IN f_number INT(11),IN d_date DATE)
-BEGIN
-	DELETE 
-	FROM cur_flight_sort_plan 
-	WHERE ( flight_number = f_number AND departure_date = d_date);
-END
