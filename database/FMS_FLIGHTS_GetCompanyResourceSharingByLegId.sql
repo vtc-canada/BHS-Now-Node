9174f97239cf784f82bfc522d4ee3424f2115103
diona <admin@ISYS-DEV-ACD.ISYSNOW.net>
2015-02-26 12:58:19 -0500

    Working on Flight Sharing seats.

--- database/FMS_FLIGHTS_GetCompanyResourceSharingByLegId.sql
+++ database/FMS_FLIGHTS_GetCompanyResourceSharingByLegId.sql
@@ -4,7 +4,8 @@ DROP procedure IF EXISTS `FMS_FLIGHTS_GetCompanyResourceSharingByLegId`;
 DELIMITER $$
 CREATE DEFINER=`root`@`%` PROCEDURE `FMS_FLIGHTS_GetCompanyResourceSharingByLegId`(IN legId INT(11))
 BEGIN
-	SELECT cur_leg_resource_sharing.id, cur_leg_resource_sharing.cur_legs_id, cur_leg_resource_sharing.cur_company_id, cur_leg_resource_sharing.seats
+	SELECT cur_leg_resource_sharing.id, cur_leg_resource_sharing.cur_legs_id, cur_leg_resource_sharing.cur_company_id, cur_leg_resource_sharing.seats, cur_company.name
 FROM cur_leg_resource_sharing
+	INNER JOIN cur_company ON cur_company.id = cur_leg_resource_sharing.cur_company_id
 WHERE cur_legs_id = legId;
 END$$
--- database/GetContactsByName.sql
+++ database/GetContactsByName.sql
@@ -7,6 +7,7 @@ BEGIN
 SELECT
 	
 	cur_contacts.id AS 'contact_id'
+	,cur_contacts.cur_company_id
 	,cur_contacts.first_name AS 'first_name'
 	,cur_contacts.last_name AS 'last_name'
 	,CONCAT_WS(' ',CONCAT_WS(', ',cur_contacts.last_name,cur_contacts.first_name), cur_contacts.middle_name) AS 'contact_name'
@@ -14,8 +15,7 @@ SELECT
 	,cur_contacts.phone_number
 	,cur_company.name as 'company'
 FROM cur_contacts
-	LEFT JOIN cur_contact_company_mapping ON (cur_contact_company_mapping.cur_contacts_id = cur_contacts.id)
-	LEFT JOIN cur_company ON (cur_company.id = cur_contact_company_mapping.cur_company_id and cur_company.is_deleted = 0)
+	LEFT JOIN cur_company ON (cur_company.id = cur_contacts.cur_company_id and cur_company.is_deleted = 0)
 WHERE 
 	cur_contacts.is_deleted = 0
 	AND (contactSearchTerms IS NULL OR MATCH (cur_contacts.first_name,cur_contacts.last_name,cur_contacts.middle_name
