382d04ff28ebc41db709bd72de667a9efeff8829
wiechars <rwiecha@isystemsnow.com>
2015-03-03 16:32:23 -0500

    Fixed formatting of query.

--- database/FMS_FLIGHTS_CreateLeg.sql
+++ database/FMS_FLIGHTS_CreateLeg.sql
@@ -1,13 +1,28 @@
-
-DROP procedure IF EXISTS `FMS_FLIGHTS_CreateLeg`;
+DROP PROCEDURE IF EXISTS `FMS_FLIGHTS_CreateLeg`;
 
 DELIMITER $$
-CREATE PROCEDURE `FMS_FLIGHTS_CreateLeg` (IN paramFlight_id INT,IN paramAirline INT, IN paramFlight_number INT, IN paramDeparture_time DATETIME, IN paramArrival_time DATETIME,  IN paramOrigin_airport_code INT, IN paramDestination_airport_code INT, IN paramAirplaneId INT, OUT paramInsertId INT)
+CREATE PROCEDURE `FMS_FLIGHTS_CreateLeg` (IN flightID INT,IN airline INT, IN flightNumber INT
+				, IN departureTime DATETIME, IN arrivalTime DATETIME,IN originAirportCode INT
+				, IN destinationAirportCode INT, IN airplaneID INT, OUT insertID INT)
 BEGIN
-	INSERT INTO cur_legs (`flight_id`, `airline`, `flight_number`, `departure_time`, `arrival_time`, `origin_airport_code`, `destination_airport_code`, `cur_airplanes_id`)
+	INSERT INTO cur_legs (flight_id
+			,airline
+			,flight_number
+			,departure_time
+			,arrival_time
+			,origin_airport_code
+			,destination_airport_code
+			,cur_airplanes_id)
 VALUES
-	(paramFlight_id, paramAirline, paramFlight_number, paramDeparture_time, paramArrival_time, paramOrigin_airport_code, paramDestination_airport_code, paramAirplaneId);
-SET paramInsertId = LAST_INSERT_ID();
+	(flightID
+	,airline
+	,flightNumber
+	,departureTime
+	,arrivalTime
+	,originAirportCode
+	,destinationAirportCode
+	,airplaneID);
+SET insertID = LAST_INSERT_ID();
 END$$
 
 DELIMITER ;
