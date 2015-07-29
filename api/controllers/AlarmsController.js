421835b1561b876d1451a3f1ec1cbefae32fa62d
andydion <adion@isystemsnow.com>
2015-07-29 14:37:20 -0400

    Alarms Controller missing from BHS branch - Fix

--- api/controllers/AlarmsController.js
+++ api/controllers/AlarmsController.js
@@ -24,18 +24,20 @@ var AlarmsController = {
   // OPEN ACCESS function -
   // called from equipment status manager - triggering pushes to everyone subscribed to alarms.
   pushAlarms : function(req,res) {
-    sails.controllers.database.sp("BHS_ALARMS_GetAlarms", [], function(err,
-        curAlarms) {
-      if (typeof (sails.io.rooms[ALARMS_ROOM]) == 'undefined') {
-        res.json('none updated');
-        return;
-      }
-      for (var curSession = 0; curSession < sails.io.rooms[ALARMS_ROOM].length; curSession++) {
-        sails.io.sockets.sockets[sails.io.rooms[ALARMS_ROOM][curSession]].emit(ALARMS_ROOM_CLIENT,
-            curAlarms);
-      }
-      res.json('success');
-    });
+    Database.dataSproc("BHS_ALARMS_GetAlarms", [], function(err, curAlarms) {
+	    if (err) {
+		res.json({
+		    error : "DB Error"
+		});
+		return;
+	    }
+	    if (typeof (sails.io.rooms['/alarms']) == 'undefined' || sails.io.rooms['/alarms'].length <= 0) {
+		res.json('none updated');
+		return;
+	    }
+	    sails.io.sockets.emit('alarms', curAlarms[0]);
+	    res.json('success');
+	});
   },
   // joins clients to the alarms room.
   joinAlarms : function(req,res) {
